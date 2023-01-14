6715d490a4f886add87967503d595be143b3a334
Ahmed <ahmed_hany19@outlook.com>
2023-01-14 23:39:59 +0200

    Component Rockt & Smoke

---
+++ src/Components/Rocket/Smoke/index.js
@@ -0,0 +1,9 @@
+const Smoke = () => {
+  return (
+    <div>
+      <h1>Smoke</h1>
+    </div>
+  );
+};
+
+export default Smoke;
---
+++ src/Components/Rocket/index.js
@@ -0,0 +1,9 @@
+const Rocket = () => {
+  return (
+    <div>
+      <h1>Rocket</h1>
+    </div>
+  );
+};
+
+export default Rocket;
