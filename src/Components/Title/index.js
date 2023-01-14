ac204511845b103bf5c7ce2f7ea6157b81b7ddb5
Ahmed <ahmed_hany19@outlook.com>
2023-01-14 23:38:49 +0200

    Component Title

---
+++ src/Components/Title/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from "./Title.module.css";
+
+const Title = () => {
+  return (
+    <div>
+      <h1 className={Styles.title}>Nasa Facts</h1>
+    </div>
+  );
+};
+
+export default Title;
